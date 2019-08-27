4a4fb13caaf9e2a9479021be6fb266ecf6275fee
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-27 20:30:58 +0800

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './componets/Navbar';
@@ -11,11 +12,14 @@ import Default from './componets/Default';
 function App() {
   return (
     <React.Fragment>
+
       <Navbar></Navbar>
-      <ProductList></ProductList>
-      <Details></Details>
-      <Cart></Cart>
-      <Default></Default>
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
     </React.Fragment>
   );
 }
--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import {logo} from '../logo.svg'
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
+            
+        <nav className = "navbar navbar-expand-sm bg-secondary navbar-dark px-sm-5">
+            <Link to='/' >
+            <i class="fas fa-home"></i>
+            </Link>
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                        products
+                    </Link>
+                </li>
+            </ul>
+            <Link to="/cart" className="ml-auto">
+                <button>
+                    <i className="fas fa-cart-plus"/>
+                    my cart
+                </button>
+            </Link>
+        </nav>    
         )
     }
 }
