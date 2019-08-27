709f7fb0ec3a07df81c2daeb48713864f0a1a733
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-27 21:12:56 +0800

    Title created awsome

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import {logo} from '../logo.svg'
+import logo from '../logo.svg'
+import styled from 'styled-components';
 
 export default class Navbar extends Component {
     render() {
@@ -8,6 +9,7 @@ export default class Navbar extends Component {
             
         <nav className = "navbar navbar-expand-sm bg-secondary navbar-dark px-sm-5">
             <Link to='/' >
+               
             <i class="fas fa-home"></i>
             </Link>
             <ul className="navbar-nav align-items-center">
@@ -18,12 +20,23 @@ export default class Navbar extends Component {
                 </li>
             </ul>
             <Link to="/cart" className="ml-auto">
-                <button>
-                    <i className="fas fa-cart-plus"/>
+                <ButtonContainer>
+                    <span className="mr-2"><i className="fas fa-cart-plus"/></span>
                     my cart
-                </button>
+                </ButtonContainer>
             </Link>
         </nav>    
         )
     }
 }
+
+const ButtonContainer = styled.button`
+    text-transform: capitalize;
+    font-size: 1.4rem;
+    background: transparent;
+    border:0.05rem solid var(--lightBlue);
+    cursor:pointer;
+    padding:0.2rem 0.5rem;
+    margin:0.2rem 0.5rem 0.2rem 0;
+
+`;
--- src/componets/ProductList.js
+++ src/componets/ProductList.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import Product from "./Product";
+import Title from './Title';
 export default class ProductList extends Component {
     render() {
         return (
-            <Product />
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="products" />
+                        <div className="row">
+
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
