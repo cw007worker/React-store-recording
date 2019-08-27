3dfad0db4750b890b3eb03121cb395380e4e31a3
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-27 13:48:11 +0800

    Cart, Default, Details, Navbar, Product, ProductList components created

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import Navbar from './componets/Navbar';
+import ProductList from './componets/ProductList';
+import Details from './componets/Details';
+import Cart from './componets/Cart';
+import Default from './componets/Default';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">column number one</div>
-        <div className="col-6"><i className="fas fa-home" /></div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
+    </React.Fragment>
   );
 }
 
---
+++ src/componets/ProductList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Product from "./Product";
+export default class ProductList extends Component {
+    render() {
+        return (
+            <Product />
+        )
+    }
+}
