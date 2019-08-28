306ada3a42428ef5913e3fc89496cf96d8bcd511
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-28 16:20:36 +0800

    cart-footer added

--- src/componets/Product.js
+++ src/componets/Product.js
@@ -13,7 +13,19 @@ export default class Product extends Component {
                     <Link to = "/details">
                         <img src = {img} alt = "product" className="card-img-top"/>
                     </Link>
-                    <button className = "cart-btn" disabled = {inCart? true: false}/>
+                    <button className = "cart-btn" disabled = {inCart? true: false} onClick={()=>console.log('added to the cart')}>
+                    {inCart?(<p className="text-capitalize mb-0" disabled> in inCart</p>):(<i className="fas fa-cart-plus"/>)}
+                    </button>
+                    </div>
+                    {/* card footer*/}
+                    <div className = "card-footer d-flex justify-content-between">
+                        <p className = "align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className = "text-blue font-italic mb-0">
+                            <span className = "mr-1">$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
             </ProductWrapper>
