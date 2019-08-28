f88b033a66e5fb68c779ba0a34fe0ea770115e62
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-28 17:00:47 +0800

    ProductWrapper styled component css completd

--- src/componets/Product.js
+++ src/componets/Product.js
@@ -34,4 +34,47 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
-`
+.card{
+    border-color:transparent;
+    transition:all 1s linear;
+}
+.card-footer{
+    background:transparent;
+    border-top:transparent;
+    transition:all 1s linear;
+}
+&:hover{
+    .card{
+        border:0.04rem solid rgba(0,0,0,0.2);
+        box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .car-footer{
+        background: rgba(247, 247, 247);
+    }
+}
+.img-container{
+    position:relative;
+    overflow:hidden;
+}
+.card-img-top{
+    transition:all 0.5s linear;
+}
+.img-container:hover .card-img-top{
+    transform: scale(1.55);
+}
+.cart-btn{
+    position:absolute;
+    bottom:0;
+    right:0;
+    padding:0.2rem 0.4rem;
+    background:var(--lightBlue);
+    border:none;
+    color:var(mainWhite);
+    border-radius:0.5rem 0 0 0;
+    transform:translate(100%,100%);
+    transition:all 0.5s linear;
+}
+.img-container:hover .cart-btn{
+    transform:translate(0%,0%);
+}
+`;
