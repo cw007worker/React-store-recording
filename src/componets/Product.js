8da171937aaee8b580eb73d8f1787360edfdce34
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-28 16:03:05 +0800

    button added in product box

--- src/componets/Product.js
+++ src/componets/Product.js
@@ -13,6 +13,7 @@ export default class Product extends Component {
                     <Link to = "/details">
                         <img src = {img} alt = "product" className="card-img-top"/>
                     </Link>
+                    <button className = "cart-btn" disabled = {inCart? true: false}/>
                     </div>
                 </div>
             </ProductWrapper>
